ce8328811f43ebf507f8091684a7339a6c5aac2b
juancarlosfernandez39110-gif <juancarlosfernandez39110@gmail.com>
2026-03-23 14:44:46 +0100

    primer commit de la estructura del proyecto

---
+++ proyectoCalculadoraN/src/main/java/com/mycompany/proyectocalculadoran/ProyectoCalculadoraN.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectocalculadoran;
+
+/**
+ *
+ * @author juanc
+ */
+public class ProyectoCalculadoraN {
+
+    public static void main(String[] args) {
+        System.out.println("=== Calculadora Versión 1.0 ===");
+        System.out.println("Desarrollada con Git y GitHub");
+        
+        // Ejemplo de suma
+        int resultado = sumar(5, 3);
+        System.out.println("5 + 3 = " + resultado);
+    }
+    
+    public static int sumar(int a, int b) {
+        return a + b;
+    }
+    }
+
